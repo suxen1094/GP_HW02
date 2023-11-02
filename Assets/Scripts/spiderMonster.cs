74afd6c276c6a3561467d14c23fb5a0b630f83b0
mr600hans <hieshhanson1@gmail.com>
2023-11-02 17:48:32 +0800

    Scene_Hans

--- Assets/Scripts/spiderMonster.cs
+++ Assets/Scripts/spiderMonster.cs
@@ -5,14 +5,10 @@ using UnityEngine.AI;
 
 public class spiderMonster : MonoBehaviour
 {
-    public AudioClip injuriedAudioClip;
-    public AudioSource audioPlayer;
-    public GameObject injuriedParticleSystem;
     public GameObject player;
     public float rushVelocity = 500.0f;
     public float stopAttackingSeconds = 5.0f;
     public float waitForRushSeconds = 1.5f;
-    public float emitParticleSeconds = 1.0f;
     public float rangeOfSensor = 500.0f;
     public float rangeOfAttack = 20.0f;
     public float maxHp = 10.0f;
@@ -32,7 +28,6 @@ public class spiderMonster : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(emitParticleForSeconds());
         naviAgent = this.GetComponent<NavMeshAgent>();
         animator = this.GetComponent<Animator>();
         originSpiderPos = this.GetComponent<Transform>().position;
@@ -94,21 +89,14 @@ public class spiderMonster : MonoBehaviour
         isStopAttacking = false;
     }
 
-    IEnumerator emitParticleForSeconds()
-    {
-        Vector3 particlePos = this.GetComponent<Transform>().position;
-        particlePos.y += 5.0f;
-        GameObject test = GameObject.Instantiate(injuriedParticleSystem, particlePos, Quaternion.Euler (270, 0, 0));
-        yield return new WaitForSeconds(emitParticleSeconds);
-        Destroy(test, 0.0f);
-    }
-
     void beAttacked(float damage)
     {
         hp -= damage;
-        audioPlayer.PlayOneShot(injuriedAudioClip);
-        StartCoroutine(emitParticleForSeconds());
-        if(hp <= 0)
+        if(hp > 0.0f)
+        {
+            //受傷聲音與粒子效果
+        }
+        else
         {
             //死亡聲音與粒子效果
             Destroy(this, destroyingSeconds);
